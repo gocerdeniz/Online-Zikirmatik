c7260934ac66d2b7d4b706ae1e1ed84d8679e048
gocerdeniz <61621796+gocerdeniz@users.noreply.github.com>
2020-05-02 16:06:17 +0300

    cc

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,19 @@ let zikirmatikCounter = document.getElementById("zikirmatikCounter");
 let zikirmatikCounterIncrement = document.getElementById("increment");
 let zikirmatikCounterDecrement = document.getElementById("decrement");
 let zikirmatikCounterReset = document.getElementById("reset");
+let zikirmatikCounterSave = document.getElementById("save");
 
-function incrementCounter() {
+document.addEventListener('keydown', incrementCounter);
+
+
+function incrementCounter(e) {
   let zikirmatikCounterValue = parseInt(zikirmatikCounter.innerHTML);
+  if ((zikirmatikCounterValue < arr[arrIndex] || arr.length == 0) || e.code == "ArrowRight") {
   zikirmatikCounterValue = zikirmatikCounterValue + 1;
   zikirmatikCounter.innerHTML = zikirmatikCounterValue;
+} else {
+  alert("Limite ulaştınız.")
+}
 }
 
 function decrementCounter() {
@@ -22,13 +30,68 @@ function resetCounter() {
   zikirmatikCounterValue = 0;
   zikirmatikCounter.innerHTML = zikirmatikCounterValue;
 }
-
-
+// audios
 let bruh = new Audio();
 bruh.src = "./movie_1.mp3";
 
 let supriseMotherfucker = new Audio();
-supriseMotherfucker.src= "./movie_2.mp3";
+supriseMotherfucker.src = "./movie_2.mp3";
 
 let erro = new Audio();
 erro.src = "./erro.mp3";
+
+// Saving Counter Values and Pushing them to table
+let savedCounterValues = new Set();
+
+function saveCounter() {
+  let zikirmatikCounterValue = parseInt(zikirmatikCounter.innerHTML);
+  savedCounterValues.add(zikirmatikCounterValue);
+  function pushToTable() {
+    // Creating Elements
+    let tr = document.createElement("tr");
+    let th = document.createElement("th");
+    let td = document.createElement("td");
+    let td2 = document.createElement("td");
+    // Creating text nodes for elements
+    let i = savedCounterValues.size - 1;
+    let counterNode = document.createTextNode(
+      Array.from(savedCounterValues)[i]
+    );
+    let newDate = new Date();
+    let dateNode =
+      newDate.getFullYear() +
+      "-" +
+      (newDate.getMonth() + 1) +
+      "-" +
+      newDate.getDate();
+    let hourNode = newDate.getHours() + ":" + newDate.getMinutes();
+    // Appending text nodes to elements
+    th.appendChild(counterNode);
+    td.innerHTML = dateNode;
+    td2.innerHTML = hourNode;
+    // Appending elements to each other
+    tr.appendChild(th);
+    tr.appendChild(td);
+    tr.appendChild(td2);
+    // Appending entire row to table's body
+    document.getElementById("tbody").appendChild(tr);
+  }
+  pushToTable();
+}
+
+let arr = [];
+let arrIndex = arr.length;
+function limitCounter(){
+  limit = parseInt(prompt("Lütfen limit giriniz..."));
+  let zikirmatikCounterValue = parseInt(zikirmatikCounter.innerHTML);
+  console.log(limit);
+  arr.push(limit);
+}
+
+function removeCounterLimit() {
+  if(arr.length == 0) {
+    alert("Limit bulunmamaktadır...")
+  } else {
+  arr = [];
+  alert("Limit başarıyla kaldırıldı...")}
+}
